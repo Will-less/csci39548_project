51792a255fae6bf4d7221d61432144ca94e83db8
Willis Yu <willisyu754@gmail.com>
2026-03-31 16:07:32 -0400

    giving up on the extension

--- my-app/src/Pages/Texthooker.jsx
+++ my-app/src/Pages/Texthooker.jsx
@@ -1,8 +1,34 @@
+import { useState, useEffect } from 'react';
+
 function Texthooker() {
+	//change to array later; we will define page size with indices
+  const [text, setText] = useState('');
+
+  const updateText = async () => {
+    try {
+      const text = await navigator.clipboard.readText();
+      setText(text);
+    } catch (e) {
+		//empty to prevent console spam
+    }
+  };
+
+	//add textractor websocket
+	
+
+  useEffect(() => {
+    window.addEventListener('focus', updateText);
+
+    return () => {
+      window.removeEventListener('focus', updateText);
+    };
+  }, []);
+
   return (
     <>
       <div>
-      </div>
+	 	 {text}
+	  </div>
     </>
   )
 }
