32b65bc9e20a0a5eb284c1dfabd173437b19ee0b
Willis Yu <willisyu754@gmail.com>
2026-03-31 17:09:04 -0400

    textractor websocket compatibility added

--- my-app/src/Pages/Texthooker.jsx
+++ my-app/src/Pages/Texthooker.jsx
@@ -1,7 +1,9 @@
 import { useState, useEffect } from 'react';
 
+const URL = "ws://localhost:6677";
+
 function Texthooker() {
-	//change to array later; we will define page size with indices
+  //change to array later; we will define page size with indices
   const [text, setText] = useState('');
 
   const updateText = async () => {
@@ -9,12 +11,26 @@ function Texthooker() {
       const text = await navigator.clipboard.readText();
       setText(text);
     } catch (e) {
-		//empty to prevent console spam
+      //empty to prevent console spam
     }
   };
 
-	//add textractor websocket
-	
+  useEffect(() => {
+    const socket = new WebSocket(URL);
+
+    socket.onopen = () => {
+      console.log("it works");
+    };
+
+    socket.onmessage = (e) => {
+      setText(e.data);
+    }
+
+    socket.onerror = () => {
+      console.log("starting manual mode");
+    }
+
+  }, []);
 
   useEffect(() => {
     window.addEventListener('focus', updateText);
@@ -27,8 +43,8 @@ function Texthooker() {
   return (
     <>
       <div>
-	 	 {text}
-	  </div>
+        {text}
+      </div>
     </>
   )
 }
