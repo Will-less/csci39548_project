c56c3a69f0c0bb3d1f37d03d92d3e98cbfdaf4f0
Willis Yu <willisyu754@gmail.com>
2026-04-14 20:01:44 -0400

    changed page state

--- my-app/src/Pages/Texthooker.jsx
+++ my-app/src/Pages/Texthooker.jsx
@@ -92,7 +92,8 @@ function usePaginator({ text, textRef, page, setPage, setPages }) {
     console.log(oh);
 
     if (sh > oh) {
-      const newPage = page + 1;
+      const newNum = page.num + 1;
+      const newPage = { id: crypto.randomUUID(), num: newNum };
       setPage(newPage);
       setPages(prev => new Map(prev).set(newPage, text.currLine));
     }
@@ -125,10 +126,14 @@ function SaveButton() {
 function Texthooker() {
   //for Textractor connection 
   const [connected, setConnected] = useState(true);
-  const [page, setPage] = useState(0);
+  //TODO: convert pages to json object - add to pages map 
+  const [page, setPage] = useState(() => ({
+    id: crypto.randomUUID(),
+    num: 0,
+  }));
 
-  //page index : line offset - offset is changed by the paginator 
-  const [pages, setPages] = useState(new Map([[1, 0]]));
+  //page : line offset - offset is changed by the paginator 
+  const [pages, setPages] = useState(new Map([[page, 0]]));
   //separate array containing the text of the page specifically 
 
   //lines contains every extracted line of text 
@@ -151,12 +156,12 @@ function Texthooker() {
   //contains text of a page that is a slice of all the text 
   const pageText = text.lineIds.slice(pageStart, pageStart + pageLines);
 
-  //add page ids
+  //TODO: add page ids
   return (
     <>
       <div className="flex justify-center ">
         <div className="flex-1">
-          current page: {page}
+          current page: {page.num}
           <div>total lines: {text.lineIds.length}</div>
           currentLine: {text.currLine}
         </div>
@@ -167,7 +172,7 @@ function Texthooker() {
         </div>
         <div className="flex-1">
           {Array.from(pages).map(([page, offset]) => (
-            <div key={page}><button>{page} - {offset}</button></div>
+            <div key={page.id}><button>{page.num + 1} - {offset}</button></div>
           ))}
         </div>
       </div>
