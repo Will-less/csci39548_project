ed81bb3813d0eb1f338c1241712bf1d4fec078f3
Willis Yu <willisyu754@gmail.com>
2026-05-16 23:09:47 -0400

    Schema for text

--- my-app/src/Backend/Text.js
+++ my-app/src/Backend/Text.js
@@ -1,8 +1,32 @@
 import mongoose from 'mongoose';
 
+const pageSchema = new mongoose.Schema({
+  pageId: Schema.Types.UUID,
+  offset: Number
+})
+
+const Page = mongoose.model("Page", pageSchema);
+
 const textSchema = new mongoose.Schema({
-  title: String,
-  content: String
+  title: {
+    type: String,
+    default: 'Untitled Document'
+  },
+  textContent: {
+    lineIds: [Schema.Types.UUID],
+    lines: {
+      type: Map,
+      of: new mongoose.Schema({
+        text: { type: String, required: true }
+      }, { _id: false })
+    },
+    currLine: Number
+  },
+  pages: {
+    type: [Schema.Types.ObjectId],
+    ref: Page
+  }
+
 })
 
 const Text = mongoose.model("Text", textSchema);
