6e4f57c224f3bc4c5edb9bf3524038d0886e6e3c
Willis Yu <willisyu754@gmail.com>
2026-05-17 02:25:20 -0400

    project restructuring 3

--- my-app/backend/server.js
+++ my-app/backend/server.js
@@ -2,7 +2,8 @@ import express from "express";
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import cors from 'cors';
-import Text from './Text.js';
+import Text from './schema/Text.js';
+
 
 dotenv.config({ path: './config.env' });
 
